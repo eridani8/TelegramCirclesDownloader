add136a0eb183b818732690b85013bd77c4c5b61
Epsilon Eridani <tsuuzetsu@gmail.com>
2025-03-23 01:54:45 +0300

    init

---
+++ TelegramDownloader/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Spectre;
+using TelegramDownloader.Service;
+
+const string outputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+var logsPath = Path.Combine(AppContext.BaseDirectory, "logs");
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Spectre(outputTemplate)
+    .WriteTo.File($"{logsPath}/errors-.log", rollingInterval: RollingInterval.Day, outputTemplate: outputTemplate, restrictedToMinimumLevel: LogEventLevel.Error)
+    .CreateLogger();
+
+try
+{
+    var builder = Host.CreateApplicationBuilder();
+    
+    builder.Services.AddSerilog();
+    builder.Services.AddHostedService<ConsoleMenu>();
+    
+    var app = builder.Build();
+
+    await app.RunAsync();
+}
+catch (Exception e)
+{
+    Log.ForContext<Program>().Fatal(e, "Приложение не может загрузиться");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
---
+++ TelegramDownloader/Service/ConsoleMenu.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Spectre.Console;
+
+namespace TelegramDownloader.Service;
+
+public class ConsoleMenu(IHostApplicationLifetime lifetime) : IHostedService
+{
+    private Task? _task;
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _task = Worker();
+        return Task.CompletedTask;
+    }
+    
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            lifetime.StopApplication();
+            if (_task != null)
+            {
+                await Task.WhenAny(_task, Task.Delay(Timeout.Infinite, cancellationToken));
+            }
+        }
+        finally
+        {
+            _task?.Dispose();
+        }
+    }
+
+    private Task Worker()
+    {
+        const string auth = "Авторизация";
+        const string download = "Загрузка";
+        const string convert = "Конвертер";
+        const string exit = "Выход";
+        
+        while (!lifetime.ApplicationStopping.IsCancellationRequested)
+        {
+            var menu = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(
+                        auth,
+                        download,
+                        convert,
+                        exit
+                    ));
+            
+            try
+            {
+                switch (menu)
+                {
+                    case exit:
+                        lifetime.StopApplication();
+                        break;
+                }
+            }
+            catch(Exception e)
+            {
+                Log.ForContext<ConsoleMenu>().Error(e, "При выборе элемента меню возникла ошибка");
+                AnsiConsole.WriteException(e);
+            }
+        }
+        
+        return Task.CompletedTask;
+    }
+}
